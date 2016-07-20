c49e11f3f1dd20c94e0b1307c4fa3f11f328912e
Apoorva-A <aa3vs@virginia.edu>
2016-07-19 22:35:39 -0400

    Filled in About/HowTo pages

--- app/src/main/java/aa3vs/cs2110/virginia/edu/cardiosafe/About.java
+++ app/src/main/java/aa3vs/cs2110/virginia/edu/cardiosafe/About.java
@@ -1,7 +1,61 @@
 package aa3vs.cs2110.virginia.edu.cardiosafe;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by Apoorva on 7/19/2016.
  */
-public class About {
+public class About extends ActionBarActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+        final Button homeButton = (Button) findViewById(R.id.mainButton);
+
+
+        homeButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        goHome(homeButton);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Will open up either run or water page
+
+    public void goHome(View homeButton) {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+    }
 }
